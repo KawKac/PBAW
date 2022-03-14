31b0358ef7b19aae8896ff6871206812c6211631
Kacper <kacper.jerzy.kawala@gmail.com>
2022-03-14 15:47:57 +0100

    Lekcja 1 v3

--- app/calc.php
+++ app/calc.php
@@ -2,23 +2,23 @@
 require_once dirname(__FILE__).'/../config.php';
 $x = $_REQUEST ['x'];
 $y = $_REQUEST ['y'];
-if ( ! (isset($x) && isset($y)) {
+if ( ! (isset($x) && isset($y))) {
 	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
 }
 
 if ( $x == "") {
-	$messages [] = 'Nie podano liczby 1';
+	$messages [] = 'Nie podano liczby kwoty do pożyczenia';
 }
 if ( $y == "") {
-	$messages [] = 'Nie podano liczby 2';
+	$messages [] = 'Nie podano liczby rat';
 }
 if (empty( $messages )) {
 	if (! is_numeric( $x )) {
-		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+		$messages [] = 'Proszę wprowadzić liczbę całkowitą w polu kwoty pożyszki';
 	}
 
 	if (! is_numeric( $y )) {
-		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
+		$messages [] = 'Proszę wprowadzić liczbę całkowitą w polu ilości rat';
 	}
 }
 

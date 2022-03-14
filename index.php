864abac63c6afff1e57a52b881945ca9b877282f
Kacper <kacper.jerzy.kawala@gmail.com>
2022-03-14 15:29:18 +0100

    Lekcja 1 v2

--- index.php
+++ index.php
@@ -1,30 +1,3 @@
-<!DOCTYPE html>
-<html lang="pl" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>PBAW</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  </head>
-  <body>
-    <form class="calc" action="index.php" method="get">
-      <h1>Przelicz swój kredyt</h1>
-      <div class="form-group">
-        <label for="howtake">
-          Pożyczasz
-        </label>
-        <input type="text" name="how_many" value="" placeholder="Wprowadź kwotę" class="form-control" value="<?php print $money ?>">
-      </div>
-      <div class="form-group">
-        <label for="howback">
-          Oddajesz
-        </label>
-        <input type="text" name="how_back" value="" placeholder="Oddajesz" class="form-control">
-      </div>
-      <input type="submit" name="lets_calc" value="Przelicz" class="btn btn-success">
-    </form>
-  </body>
-</html>
 <?php
-
- ?>
+require_once dirname(__FILE__).'/config.php';
+include _ROOT_PATH.'/app/calc_view.php';
